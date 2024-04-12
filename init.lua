6815ae00d9e0b78927f82e52faf48d0d4dd68c14
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2024-04-12 20:46:15 +0700

    :sparkles: ft: add qqi to escape when needed
    may as well add qqa

--- init.lua
+++ init.lua
@@ -38,12 +38,7 @@ vim.api.nvim_set_keymap("i", "qq", "<Esc>", { noremap = true, silent = true, des
 vim.api.nvim_set_keymap("i", "yyy", "<Esc>yy", { noremap = true, silent = true, desc = "Escape and yank." })
 vim.api.nvim_set_keymap("i", "ddd", "<Esc>dd", { noremap = true, silent = true, desc = "Escape and delete line." })
 vim.api.nvim_set_keymap("i", "vvv", "<Esc>v$", { noremap = true, silent = true, desc = "Escape and Visual line." })
-vim.api.nvim_set_keymap(
-  "t",
-  "qqq",
-  "<C-\\><C-n>",
-  { noremap = true, silent = true, desc = "Normal mode from Terminal mode" }
-)
+vim.api.nvim_set_keymap("t", "qqq", "<C-\\><C-n>", { noremap = true, silent = true, desc = "Normal mode from Terminal mode" })
 vim.api.nvim_set_keymap(
   "t",
   "qqi",
@@ -59,6 +54,13 @@ vim.api.nvim_set_keymap(
   { noremap = true, silent = true, desc = "Escape and insert in Terminal mode" }
 )
 >>>>>>> Stashed changes
+
+vim.api.nvim_set_keymap(
+  "t",
+  "qqa",
+  "<Esc>i",
+  { noremap = true, silent = true, desc = "Escape and insert in Terminal mode" }
+)
 
 --Plugin related mapping.
 --Heavily related to plugins and astronvim core.
