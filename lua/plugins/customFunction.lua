edd4357f832b90c243937ec249f37e96f1e25b0d
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2024-04-15 23:46:14 +0700

    :sparkles: ft: insert hyperlink by <C-k>

--- lua/plugins/customFunction.lua
+++ lua/plugins/customFunction.lua
@@ -14,31 +14,32 @@ vim.keymap.set(
   { noremap = true, silent = true, desc = "Map focus" }
 )
 -- Surround function.
-function Surround(w_or_W)
-  local open_char = vim.fn.input "Surround with: "
+function Surround(w_or_W, char)
+  char = char or nil
+  local link_title = char or vim.fn.input "Surround with: "
   local closed_char = nil
-  if open_char == "(" then closed_char = ")" end
-  if open_char == "[" then closed_char = "]" end
-  if open_char == "{" then closed_char = "}" end
-  if open_char == "<" then closed_char = ">" end
-  if open_char == "'" then closed_char = "'" end
-  if open_char == '"' then closed_char = '"' end
-  if open_char == "`" then closed_char = "`" end
-  if open_char == "/" then closed_char = "/" end
-  if open_char == "|" then closed_char = "|" end
+  if link_title == "(" then closed_char = ")" end
+  if link_title == "[" then closed_char = "]" end
+  if link_title == "{" then closed_char = "}" end
+  if link_title == "<" then closed_char = ">" end
+  if link_title == "'" then closed_char = "'" end
+  if link_title == '"' then closed_char = '"' end
+  if link_title == "`" then closed_char = "`" end
+  if link_title == "/" then closed_char = "/" end
+  if link_title == "|" then closed_char = "|" end
   local current_mode = vim.fn.mode()
   if closed_char ~= nil then
     if current_mode == "n" then
       if w_or_W == "w" then
-        vim.cmd("normal! ciw" .. open_char)
+        vim.cmd("normal! ciw" .. link_title)
       elseif w_or_W == "W" then
-        vim.cmd([[normal! ciW]] .. open_char)
+        vim.cmd([[normal! ciW]] .. link_title)
       end
     elseif current_mode == "v" then
       if w_or_W == "w" then
-        vim.cmd("normal! c" .. open_char)
+        vim.cmd("normal! c" .. link_title)
       elseif w_or_W == "W" then
-        vim.cmd([[normal! c]] .. open_char)
+        vim.cmd([[normal! c]] .. link_title)
       end
     end
 
@@ -140,4 +141,64 @@ vim.api.nvim_set_keymap(
   "<Esc>:%s/<c-r>=GetVisual()<cr>//g<left><left>",
   { noremap = true, silent = true, desc = "Replace the selected text" }
 )
+
+-- Get selected text
+local function get_visual_selection()
+  local s_start = vim.fn.getpos "'<"
+  local s_end = vim.fn.getpos "'>"
+  local n_lines = math.abs(s_end[2] - s_start[2]) + 1
+  local lines = vim.api.nvim_buf_get_lines(0, s_start[2] - 1, s_end[2], false)
+  lines[1] = string.sub(lines[1], s_start[3], -1)
+  if n_lines == 1 then
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3] - s_start[3] + 1)
+  else
+    lines[n_lines] = string.sub(lines[n_lines], 1, s_end[3])
+  end
+  return table.concat(lines, "\n")
+end
+
+-- Insert hyperlink function in neovim, markdown to be specific
+function Hyperlink(opts)
+  local extra_args = opts.args
+  local link_title = vim.fn.input "Link Brief?: "
+  local closed_char = nil
+  if link_title == nil then
+    closed_char = nil
+  else
+    closed_char = "ok"
+  end
+  local link_content = vim.fn.getreg "+"
+  local link_pattern = "^http"
+  if string.match(link_content, link_pattern) then
+    link_content = link_content
+  else
+    local crop_register = string.sub(link_content, -10)
+    local warningMessage = "'" .. crop_register .. "'" .. "is not a link"
+    vim.notify(warningMessage, vim.log.levels.ERROR)
+    link_content = ""
+  end
+
+  local current_mode = vim.fn.mode()
+  if (current_mode == "i") or (current_mode == "n") then
+    vim.cmd "startinsert"
+    if closed_char == nil then
+      vim.api.nvim_feedkeys("[](" .. link_content .. ")", "t", false)
+    else
+      vim.api.nvim_feedkeys("[" .. link_title .. "](" .. link_content .. ")", "t", false)
+    end
+  else
+    -- local selected_string = get_visual_selection()
+    Surround("w", "[")
+    -- vim.api.nvim_feedkeys("lll", "i", false)
+    vim.cmd [[
+    normal l 
+    startinsert
+    ]]
+    vim.api.nvim_feedkeys("(" .. link_content .. ")", "t", false)
+  end
+
+  vim.cmd [[write]]
+end
+
+vim.keymap.set({ "n", "v", "i" }, "<C-k>", function() Hyperlink "ok" end)
 return {}
