7de676dc5e0b89733b982b4a0ebc9de1c7f4dfd7
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2024-04-17 11:24:25 +0700

    :bug: fix: unstable lua plugin on mobile.
    Now may have to resolve plenary.nvim as well

--- lua/plugins/customFunction.lua
+++ lua/plugins/customFunction.lua
@@ -72,7 +72,6 @@ vim.keymap.set(
   function() copyPath() end,
   { noremap = true, desc = "Comment the line in Insert Mode" }
 )
-
 -- vim.keymap.set("n", "<CR>", '@="m`o<C-V><Esc>``"<CR>')
 vim.keymap.set("n", "<S-CR>", '@="m`O<C-V><Esc>``"<CR>')
 -- local function addNewLines(before, count)
