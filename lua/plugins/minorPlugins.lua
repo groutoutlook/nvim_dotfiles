3b82ee13320ca9989bcbeac2162fcc7480ade0c4
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2024-04-21 13:47:29 +0700

    :wrench: config: chage keymap of mini-move

--- lua/plugins/minorPlugins.lua
+++ lua/plugins/minorPlugins.lua
@@ -7,4 +7,42 @@ return {
     },
     config = true,
   },
+  {
+    "echasnovski/mini.move",
+    keys = function(_, keys)
+      local plugin = require("lazy.core.config").spec.plugins["mini.move"]
+      local opts = require("lazy.core.plugin").values(plugin, "opts", false) -- resolve mini.clue options
+      -- Populate the keys based on the user's options
+      local mappings = {
+        { opts.mappings.line_left, desc = "Move line left" },
+        { opts.mappings.line_right, desc = "Move line right" },
+        { opts.mappings.line_down, desc = "Move line down" },
+        { opts.mappings.line_up, desc = "Move line up" },
+        { opts.mappings.left, desc = "Move selection left", mode = "v" },
+        { opts.mappings.right, desc = "Move selection right", mode = "v" },
+        { opts.mappings.down, desc = "Move selection down", mode = "v" },
+        { opts.mappings.up, desc = "Move selection up", mode = "v" },
+      }
+      mappings = vim.tbl_filter(function(m) return m[1] and #m[1] > 0 end, mappings)
+      return vim.list_extend(mappings, keys)
+    end,
+    opts = {
+      mappings = {
+        left = "<A-Left>",
+        right = "<A-Right>",
+        down = "<A-Down>",
+        up = "<A-Up>",
+        line_left = "<A-Left>",
+        line_right = "<A-Right>",
+        line_down = "<A-Down>",
+        line_up = "<A-Up>",
+      },
+    },
+  },
+  {
+    "catppuccin/nvim",
+    optional = true,
+    ---@type CatppuccinOptions
+    opts = { integrations = { mini = true } },
+  },
 }
