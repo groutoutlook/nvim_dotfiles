381087eec748ea02cbca61c46d5ab9c96f9dcb4a
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2024-05-14 10:15:07 +0700

    :sparkles: ft: disable notify

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -81,6 +81,9 @@ return {
         inc_rename = utils.is_available "inc-rename.nvim", -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = false, -- add a border to hover docs and signature help
       },
+      notify = {
+        enabled = false,
+      },
     })
   end,
 }
