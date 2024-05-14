bb84f2f7e72ef22b18cabcad10ef19d02047551d
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2024-05-14 13:48:39 +0700

    :sparkles: ft: remove double quote
    since it's much faster to add second alternative right in the command box.

--- lua/plugins/customFunction.lua
+++ lua/plugins/customFunction.lua
@@ -234,7 +234,7 @@ function addEspansoString(opts)
   local wordCase = opts or "strictMatch"
   local triggerString = vim.fn.input "Trigger with: " or ""
   local replaceString = vim.fn.input "Replace with: " or ""
-  vim.api.nvim_put({ '  - triggers: ["' .. triggerString .. '",' }, "l", true, false)
+  vim.api.nvim_put({ "  - triggers: [" .. triggerString .. "," }, "l", true, false)
   vim.api.nvim_put({ "    ]" }, "l", true, false)
   vim.api.nvim_put({ '    replace: "' .. replaceString .. '" ' }, "l", true, false)
   -- always true since I type some capitalized text as well.
