ae0ca80a6e108e644bbb84c0d636e0a03f9f2f3f
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2024-05-18 16:01:58 +0700

    :art: style: powershell proper shell in lua
    no vim command at this point?

--- lua/plugins/WindowsLinuxEnv.lua
+++ lua/plugins/WindowsLinuxEnv.lua
@@ -5,14 +5,15 @@
 vim.cmd["set"] "termguicolors"
 
 if vim.loop.os_uname().sysname == "Windows_NT" then
-  vim.o.shell = "pwsh.exe"
-  -- let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-  vim.cmd [[
-let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
-let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
-let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
-set shellquote= shellxquote=
-]]
+  vim.o.shell = "pwsh"
+  vim.o.shellcmdflag =
+    [[-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();`
+  $PSDefaultParameterValues['Out-File:Encoding']='utf8';`
+  Remove-Alias -Force -ErrorAction SilentlyContinue tee;]]
+  vim.o.shellredir = "2>&1 | %%{ '$_' } | Out-File %s; exit $LastExitCode"
+  vim.o.shellpipe = "2>&1 | %%{ '$_' } | tee %s; exit $LastExitCode"
+  vim.o.shellquote = ""
+  vim.o.shellxquote = ""
   require("notify").setup {
     timeout = 1000,
     -- stages = "static",
