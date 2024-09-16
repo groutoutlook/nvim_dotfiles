afc068de7df14941017463e5235f14417c273f21
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2024-09-16 17:58:21 +0700

    🚧  wip

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -10,10 +10,12 @@ vim.keymap.set("!", "<C-d>", "<Delete>") -- delete-char
 vim.keymap.set("!", "<C-h>", "<BS>") -- backward-delete-char
 vim.keymap.set("!", "<C-a>", emacsinsert.beginning_of_line)
 vim.keymap.set("!", "<C-e>", emacsinsert.end_of_line)
-vim.keymap.set("!", "<M-f>", emacsinsert.forward_word)
-vim.keymap.set("!", "<M-b>", emacsinsert.backward_word)
-vim.keymap.set("!", "<C-f>", "<Right>") -- forward-char
-vim.keymap.set("!", "<C-b>", "<Left>") -- backward-char
+-- vim.keymap.set("!", "<M-f>", emacsinsert.forward_word)
+-- vim.keymap.set("!", "<M-b>", emacsinsert.backward_word)
+-- vim.keymap.set("!", "<C-f>", "<Right>") -- forward-char
+-- vim.keymap.set("!", "<C-b>", "<Left>") -- backward-char
+vim.keymap.set("!", "<C-Up>", emacsinsert.backward_paragraph)
+vim.keymap.set("!", "<C-Down>", emacsinsert.forward_paragraph)
 -- vim.keymap.set("!", "<C-n>", "<Down>") -- next-line
 -- vim.keymap.set("!", "<C-p>", "<Up>") -- previous-line
 
