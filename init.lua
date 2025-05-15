e9bef79cecaa4eaa483a57d062a65bd6b1d11e3c
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2025-05-16 01:13:44 +0700

    feat: overhaul a large list of redundant files
    in hope of improving boot time.

--- init.lua
+++ init.lua
@@ -25,8 +25,8 @@ command! -bang WQ wq<bang>
 command! -bang Wq wq<bang>
 ]]
 if vim.fn.has "gui_running" == 1 then
-  -- vim.cmd.colorscheme "cyberdream"
-  vim.o.guifont = "Iosevka Nerd Font Propo:h13" -- text below applies for VimScript
+  vim.cmd.colorscheme "cyberdream"
+  vim.o.guifont = "Iosevka Nerd Font Propo:h18" -- text below applies for VimScript
   vim.g.transparency = 0.7
   local alpha = function() return string.format("%x", math.floor(255 * vim.g.transparency or 0.8)) end
   vim.g.neovide_transparency = 0.5
@@ -35,5 +35,5 @@ if vim.fn.has "gui_running" == 1 then
   vim.g.neovide_floating_shadow = false
   vim.g.neovide_profiler = false
 else
-  -- vim.cmd.colorscheme "cyberdream"
+  vim.cmd.colorscheme "cyberdream"
 end
--- lua/polish.lua
+++ lua/polish.lua
@@ -1,18 +1,48 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- This will run last in the setup process and is a good place to configure
+-- INFO: This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
--- Set up custom filetypes
+-- vim.filetype.add {
+--   extension = {
+--     foo = "fooscript",
+--   },
+--   filename = {
+--     ["Foofile"] = "fooscript",
+--   },
+--   pattern = {
+--     ["~/%.config/foo/.*"] = "fooscript",
+--   },
+-- }
+-- filetype function.
 vim.filetype.add {
   extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
+    tmp = "markdown",
+    ini = "markdown",
+    jrnl = "markdown",
+    zshrc = "zsh",
+    hlsl = "cpp",
+    vert = "glsl",
+    frag = "glsl",
+    geom = "glsl",
+    -- glsl = "cpp",
   },
 }
+-- INFO: shell info.
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  vim.o.shell = "pwsh"
+  vim.o.shellcmdflag = [[-Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();`
+  $PSDefaultParameterValues['Out-File:Encoding']='utf8';`
+  ]]
+  -- Remove-Alias -Force -ErrorAction SilentlyContinue tee;
+  vim.o.shellredir = "2>&1 | %%{ '$_' } | Out-File %s; exit $LastExitCode"
+  vim.o.shellpipe = "2>&1 | %%{ '$_' } | tee %s; exit $LastExitCode"
+  vim.o.shellquote = ""
+  vim.o.shellxquote = ""
+else
+  vim.o.shell = 'nu'
+  vim.o.shellcmdflag = '-c'
+  vim.o.shellquote = ""
+  vim.o.shellxquote = ""
+end
+
+return {}
